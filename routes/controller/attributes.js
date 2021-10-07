563e962c9fbd7f2b91ad9f0db739fd0cb79f272d
Soodev20 <soodev20@gmail.com>
2021-10-07 23:03:32 +0900

    FEAT: /attribute route

--- routes/controller/attributes.js
+++ routes/controller/attributes.js
@@ -1,7 +1,34 @@
-const OK = 200;
+const puppeteer = require("puppeteer");
 
-function getAttributes(req, res, next) {
-  res.status(OK).json({ result: "ok" });
+const { OK } = require("../../constants/statusCodes");
+
+async function getAttributes(req, res, next) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(req.query.inputUrl, { waitUntil: "networkidle2" });
+    await page.waitForXPath("//*[@*]");
+
+    const elements = await page.$$("*");
+    const elementStyles = [];
+
+    for (const element of elements) {
+      const style = await page.evaluate((e) => {
+        const computedStyle = window.getComputedStyle(e);
+
+        return [...computedStyle].reduce((elementStyles, property) =>
+          ( { ...elementStyles,
+            [property]: computedStyle.getPropertyValue(property) } ), {} );
+      }, element);
+      elementStyles.push(style);
+    }
+    console.log(elementStyles);
+
+    res.status(OK).send({ data: "OK" });
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = { getAttributes };
