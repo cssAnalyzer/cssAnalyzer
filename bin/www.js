bf53d48bda7fcaa505bab7708d21d5f94c6e29a5
soodev20 <soodev20@gmail.com>
2021-10-22 10:05:02 +0900

    Modify: ebExtension config

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@ const app = require("../main");
 const debug = require("debug")("cssanalyzer-server:server");
 const http = require("http");
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "8080");
 app.set("port", port);
 
 const server = http.createServer(app);
